9cc1b292d26c184bd0769a2c98ce02da692f632a
leeight <leeight@gmail.com>
2016-11-21 23:49:15 +0800

    Update java backend code

--- backend/java/src/main/java/com/baidu/inf/bce/App.java
+++ backend/java/src/main/java/com/baidu/inf/bce/App.java
@@ -2,10 +2,15 @@ package com.baidu.inf.bce;
 
 import java.io.IOException;
 import java.util.Map;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import fi.iki.elonen.NanoHTTPD;
 
 public class App extends NanoHTTPD {
+    private static final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
     public App(int port) {
         super(port);
     }
@@ -29,6 +34,10 @@ public class App extends NanoHTTPD {
     }
 
     public static void main(String args[]) throws IOException {
+        System.out.println("Java " + System.getProperty("java.version")
+            + " Development Server started at " + sdf.format(new Date()));
+        System.out.println("Listening on http://localhost:" + (Config.port + 1));
+        System.out.println("Press Ctrl-C to quit.");
         App app = new App(Config.port + 1);
         app.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
     }
