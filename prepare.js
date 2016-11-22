343886054b91b1b916468beadf326aad9043a095
leeight <leeight@gmail.com>
2016-11-22 10:25:34 +0800

    Update prepare.js

--- prepare.js
+++ prepare.js
@@ -8,8 +8,28 @@
 var fs = require('fs');
 var path = require('path');
 
+var u = require('underscore');
+var sdk = require('bce-sdk-js');
 var config = require('./config');
 
+var DEFAULT_CORS_CONFIG = {
+    allowedOrigins: ['http://*', 'https://*'],
+    allowedMethods: ['GET', 'HEAD', 'POST', 'PUT'],
+    allowedHeaders: ['*'],
+    allowedExposeHeaders: [
+      'ETag',
+      'x-bce-request-id',
+      'x-bce-next-append-offset',
+      'x-bce-object-type'
+    ],
+    maxAgeSeconds: 1800
+};
+
+/**
+ * 从 config.json 生成 PHP 的配置文件
+ *
+ * @param {string} dest PHP Config class
+ */
 function toPHPConfig(dest) {
     var code = [
         '<?php',
@@ -26,6 +46,7 @@ function toPHPConfig(dest) {
     }
     code.push('}\n');
     fs.writeFileSync(path.join(__dirname, dest), code.join('\n'));
+    console.log('Generated %s', dest);
 }
 
 function getJavaType(value) {
@@ -44,6 +65,11 @@ function getJavaType(value) {
     }
 }
 
+/**
+ * 从 config.json 生成 java 的配置文件
+ *
+ * @param {string} dest Java Config class
+ */
 function toJavaConfig(dest) {
     var code = [
         'package com.baidu.inf.bce;',
@@ -63,8 +89,79 @@ function toJavaConfig(dest) {
     }
     code.push('}\n');
     fs.writeFileSync(path.join(__dirname, dest), code.join('\n'));
+    console.log('Generated %s', dest);
+}
+
+/**
+ * 往 bucket 中上传一个 crossdomain.xml 文件，在 IE 下面，使用 moxie.swf 上传文件的时候，
+ * 需要这个 crossdomain.xml 文件，角色类似 CORS 的配置
+ *
+ * @param {string} bucket The bucket name.
+ */
+function uploadCrossDomainXml(bucket) {
+    var xbos = new sdk.BosClient({
+        endpoint: 'https://bj.bcebos.com',
+        credentials: {
+            ak: config.ak,
+            sk: config.sk
+        }
+    });
+
+    xbos.setBucketCannedAcl(bucket, 'public-read')
+        .then(function () {
+            console.log('Set public-read to bos://%s', bucket);
+            var file = path.join(__dirname, 'crossdomain.xml');
+            return xbos.putObjectFromFile(bucket, 'crossdomain.xml', file);
+        })
+        .then(function () {
+            console.log('crossdomain.xml to bos://%s/crossdomain.xml', bucket);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+}
+
+/**
+ * 自动给 bucket 添加 cors 的配置
+ *
+ * @param {string} bucket The bucket name.
+ */
+function prepareCorsConfig(bucket) {
+    var xbos = new sdk.BosClient({
+        endpoint: 'https://bj.bcebos.com',
+        credentials: {
+            ak: config.ak,
+            sk: config.sk
+        }
+    });
+    var options = {
+        bucketName: bucket,
+        params: {
+            cors: ''
+        }
+    };
+    xbos.sendRequest('GET', options)
+        .then(function (response) {
+            var corsConfiguration = (response.body.corsConfiguration || []);
+            var found = u.find(corsConfiguration, item => u.isEqual(item, DEFAULT_CORS_CONFIG));
+            if (!found) {
+                corsConfiguration.push(DEFAULT_CORS_CONFIG);
+                return xbos.sendRequest('PUT', u.extend({
+                    body: JSON.stringify({
+                        corsConfiguration: corsConfiguration
+                    })
+                }, options));
+            }
+        })
+        .then(function () {
+            console.log('Add cors config to bos://%s', bucket);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 }
 
 toPHPConfig('backend/php/Config.php');
 toJavaConfig('backend/java/src/main/java/com/baidu/inf/bce/Config.java');
-
+uploadCrossDomainXml(config.bucket);
+prepareCorsConfig(config.bucket);
